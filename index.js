ec1a6e9cba07d0dac9c057a179b289fdf12477c5
morganherlocker <morgan.herlocker@gmail.com>
2014-08-05 17:36:45 -0400

    working on tile merge logic; not quite there yet

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var isInside = require('turf-inside'),
 var limits = {
   min_zoom: 1,
   max_zoom: 8
-}
+};
 
 module.exports.geojson = function(geom) {
   if(geom.type === 'Point') {
@@ -13,11 +13,13 @@ module.exports.geojson = function(geom) {
   } else if(geom.type === 'LineString') {
 
   } else if(geom.type === 'Polygon') {
-    var seed = [0,0,0]
+    var seed = [0,0,0];
 
-    var locked = []
-    splitSeek(seed, geom, locked, limits)
+    var locked = [];
+    splitSeek(seed, geom, locked, limits);
     
+    locked = mergeTiles(locked);
+
     var tileFeatures = locked.map(function(t){
         return tileToGeojson(t[0], t[1], t[2])
     });
@@ -34,13 +36,32 @@ module.exports.tiles = function(geom) {
   } else if(geom.type === 'LineString') {
 
   } else if(geom.type === 'Polygon') {
-    var seed = [0,0,0]
+    var seed = [0,0,0];
 
-    var locked = []
-    splitSeek(seed, geom, locked, limits)
+    var locked = [];
+    splitSeek(seed, geom, locked, limits);
     
+    locked = mergeTiles(locked);
+
     return locked;
   }
+}
+
+function mergeTiles(tiles){
+  var merged = [];
+
+  tiles.forEach(function(t){
+    //console.log(t[0], t[1])
+    //console.log((t[0]%2===0 && t[1]%2===0) )
+    if((t[0]%2===0 && t[1]%2===0) && hasSiblings(t, tiles)) {
+      console.log('MERGE')
+      merged.push(getParent(t));
+    } else if(!hasSiblings(t, tiles)){
+      console.log('NO MERGE')
+      merged.push(t);
+    } else {console.log()}
+  })
+  return merged;
 }
 
 
@@ -110,10 +131,40 @@ function getParent(tile){
   }
   // bottom right
   else if(tile[1]%2===0){
-    return (tile[0]-1)/2, (tile[1])/2, tile[2]-1]
+    return [(tile[0]-1)/2, (tile[1])/2, tile[2]-1]
   }
   // top right
   else {
-    return (tile[0]-1)/2, (tile[1]-1)/2, tile[2]-1]
+    return [(tile[0]-1)/2, (tile[1]-1)/2, tile[2]-1]
   }
+}
+
+function hasSiblings(tile, tiles){
+  if(
+      hasTile(tiles, [tile[0]+1, tile[1], tile[2]]) &&
+      hasTile(tiles, [tile[0]+1, tile[1]+1, tile[2]]) &&
+      hasTile(tiles, [tile[0], tile[1]+1, tile[2]])
+    ) {
+    return true;
+  } else {
+    //console.log('DOES NOT HAVE ALL SIBS')
+    return false;
+  }
+}
+
+function hasTile(tiles, tile){
+  //var tileFound = false;
+  tiles.forEach(function(t){
+    if(tilesEqual(t, tile)){
+      return true;
+    }
+  })
+}
+
+function tilesEqual(tile1, tile2) {
+  return (
+      tile1[0] === tile2[0] &&
+      tile1[1] === tile2[1] &&
+      tile1[2] === tile2[2]
+    );
 }
