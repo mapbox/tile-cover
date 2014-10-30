e6715007f2f82e3b3ec27e48da981962f85c0916
karenzshea <karenzshea@gmail.com>
2014-10-30 18:25:36 -0400

    full test

--- test/testworld.js
+++ test/testworld.js
@@ -2,29 +2,40 @@ var cover = require('../'),
     test = require('tape'),
     fs = require('fs');
     intersect = require('turf-intersect');
+    merge = require('turf-merge');
+    erase = require('turf-erase');
     
 
 test('the world', function(t){
     var countries = fs.readdirSync(__dirname+'/fixtures/world');
     countries.forEach(function(countryName){
-        var country = JSON.parse(fs.readFileSync(__dirname+'/fixtures/world/'+ countryName)); //each country file is a featureCollection, hopefully only one feature
-        if(country.features.length > 1) throw new Error('Invalid country; more than 1 feature')
+        console.log('party in ===============================>', countryName);
+        
+        var country = JSON.parse(fs.readFileSync(__dirname+'/fixtures/world/'+ countryName)); 
+        if (country.features.length > 1) throw new Error('Invalid country; more than 1 feature')
         var limits = {
             min_zoom : 6,
             max_zoom : 6
         }
         var countryGeom = country.features[0].geometry; //just the geometry from the country featureCollection
         var countryCover = cover.geojson(countryGeom, limits); // returns a feature collection of tiles 
-        var countryFeature = country.features[0]; // there should only be [0] feature
         var countryTiles = countryCover.features; // 
         
-        t.ok(countryCover, 'country geojson');
+        t.ok(countryCover, 'check tile-cover');
         
-        countryTiles.forEach(function(tile){
-            console.log(countryName)
+        countryTiles.forEach(function(tile){ // 'tile' is one feature object
             var overlap = intersect(tile, countryGeom);
-            t.notEqual(overlap.features[0].type, 'GeometryCollection')
+            t.notEqual(overlap.features[0].type, 'GeometryCollection', 'check for empty tiles');
         });
+
+        var countryBlock = merge(countryCover);
+        var countryBlockGeom = countryBlock.geometry;
+        t.ok(countryBlock, 'check tilecover merge');
+        
+        var knockout = erase(country.features[0], countryBlock);
+            console.log('erased output', knockout.geometry);
+        t.deepEqual(knockout.geometry.geometries, [], 'check for exposed land');
+
     });
     t.end();
 });
