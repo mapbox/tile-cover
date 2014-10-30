b81cd3810728a54e9473bec31d3317669092da50
karenzshea <karenzshea@gmail.com>
2014-10-30 10:55:12 -0400

    stopping short of an intersection

--- test/testworld.js
+++ test/testworld.js
@@ -1,18 +1,25 @@
 var cover = require('../'),
     test = require('tape'),
     fs = require('fs');
-
+    turf = require('turf-intersect');
 
 test('the world', function(t){
-    t.plan(179);
 
-    var countries = fs.readdirSync('./fixtures/world');
-    
-    countries.forEach(function(country){
-        var country = JSON.parse(fs.readFileSync('./fixtures/world'));
-        var countryCover = cover.geojson(country.feature.geometry, limits);
+    var countries = fs.readdirSync(__dirname+'/fixtures/world');
+    countries.forEach(function(countryFC){
+        var countryFC = JSON.parse(fs.readFileSync(__dirname+'/fixtures/world/'+ countryFC)); //each country file is a featureCollection
+        var limits = {
+            min_zoom : 6,
+            max_zoom : 6
+        }
+        var countryGeom = countryFC.features[0].geometry; //just the geometry from the country featureCollection
+        var countryTiles = cover.geojson(countryGeom, limits); 
         
-        t.ok(cover.geojson(country.feature.geometry, limits), 'country geojson');
-        console.log(countryCover);
+        t.ok(countryTiles, 'country geojson');
+        var intersect = turf.intersect(countryTiles, countryFC);
+        t.ok(intersect);
+        console.log(intersect);
+        
     });
+    t.end();
 });
