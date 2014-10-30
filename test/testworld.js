68bf17f722594df56ee61aba63ee1ee52f09f0f7
karenzshea <karenzshea@gmail.com>
2014-10-30 14:44:48 -0400

    cool got intersect working

--- test/testworld.js
+++ test/testworld.js
@@ -1,28 +1,29 @@
 var cover = require('../'),
     test = require('tape'),
     fs = require('fs');
-    turf = require('turf-intersect');
+    intersect = require('turf-intersect');
     
 
 test('the world', function(t){
 
     var countries = fs.readdirSync(__dirname+'/fixtures/world');
-    countries.forEach(function(countryFC){
-        var countryFC = JSON.parse(fs.readFileSync(__dirname+'/fixtures/world/'+ countryFC)); //each country file is a featureCollection
+    countries.forEach(function(country){
+        var country = JSON.parse(fs.readFileSync(__dirname+'/fixtures/world/'+ country)); //each country file is a featureCollection, hopefully only one feature
         var limits = {
             min_zoom : 6,
             max_zoom : 6
         }
-        var countryGeom = countryFC.features[0].geometry; //just the geometry from the country featureCollection
+        var countryGeom = country.features[0].geometry; //just the geometry from the country featureCollection
         var countryCover = cover.geojson(countryGeom, limits); // returns a feature collection of tiles 
-        var countryTiles = countryCover.features; //  
+        var countryFeature = country.features[0]; // there should only be [0] feature
+        var countryTiles = countryCover.features; // 
         
         t.ok(countryCover, 'country geojson');
         
         countryTiles.forEach(function(tile){
-            var intersect = turf.intersect(tile, countryCover);
-            t.ok(intersect);
-            console.log(intersect);
+            var overlap = intersect(tile, countryCover);
+            console.log(countryFeature.properties.name, overlap.features[0]);
+            t.ok(overlap);
         });
         
         
