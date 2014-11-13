75c3fd7c9018902a3da8e1d5b756bf526db3092b
Vladimir Agafonkin <agafonkin@gmail.com>
2014-11-13 20:48:29 +0200

    add Russia LineString to bench

--- bench.js
+++ bench.js
@@ -6,6 +6,7 @@ var building = JSON.parse(fs.readFileSync('./test/fixtures/building.geojson'));
 var line = JSON.parse(fs.readFileSync('./test/fixtures/road.geojson'));
 var point = JSON.parse(fs.readFileSync('./test/fixtures/point.geojson'));
 var russia = JSON.parse(fs.readFileSync('./test/fixtures/russia.geojson'));
+var russiaLine = {type: 'LineString', coordinates: russia.coordinates[0]};
 var zooms = [6,8,10,12,18];
 
 var suite = new Benchmark.Suite('tile-cover',{
@@ -23,7 +24,10 @@ zooms.forEach(function(zoom){
     addBench(suite, building, 'building', zoom, zoom);
 });
 zooms.slice(0, 3).forEach(function(zoom){
-    addBench(suite, russia, 'russia', zoom, zoom);
+    addBench(suite, russia, 'russia polygon', zoom, zoom);
+});
+zooms.slice(0, 3).forEach(function(zoom){
+    addBench(suite, russiaLine, 'russia polyline', zoom, zoom);
 });
 
 suite.on('cycle', function(event) {
